2c669a9b7d1263f319ae67577e411faa1066f5c4
aaronkeith <aflisik@gmail.com>
2017-10-25 15:18:30 -0700

    Click event listeners allow user to change slides #1

--- public/main.js
+++ public/main.js
@@ -1,5 +1,14 @@
-const imageFetch = () => {
-  return fetch('/carousel/images').then(res => res.json())
+const render = () => {
+  console.log(carousel)
+
+  if (carousel.current < carousel.slides.length - 1) {
+    carousel.current++
+  } else {
+    carousel.current = 0
+  }
+  document
+    .querySelector('.image')
+    .setAttribute('src', `${carousel.slides[carousel.current].url}`)
 }
 
 const carousel = {
@@ -7,34 +16,33 @@ const carousel = {
   current: 0
 }
 
-imageFetch().then(documents => (carousel.slides = documents))
-
-const render = () => {
-  document
-    .querySelector('.image')
-    .setAttribute('src', `${carousel.slides[carousel.current].url}`)
-  if (carousel.current < carousel.slides.length - 1) {
-    carousel.current++
-  } else {
-    carousel.current = 0
-  }
+const imageFetch = () => {
+  return fetch('/carousel/images')
+    .then(res => res.json())
+    .then(documents => (carousel.slides = documents))
+    .then(render)
 }
 
-const changeSlide = () => {
-  document
-    .querySelector('.slide')
-    .setAttribute('src', `images/${carousel.slides[carousel.current].image}`)
+document.querySelector('.button.previous').addEventListener('click', () => {
+  if (carousel.current === 0) return
+  carousel.current--
+  changeSlide()
+  console.log('previous')
+})
 
-  document.querySelector('.button.previous').addEventListener('click', () => {
-    if (carousel.current === 0) return
-    carousel.current--
-    changeSlide()
-  })
-}
 document.querySelector('.button.next').addEventListener('click', () => {
   if (carousel.current === carousel.slides.length - 1) return
   carousel.current++
   changeSlide()
+  console.log('next')
 })
+
+const changeSlide = () => {
+  document
+    .querySelector('.image')
+    .setAttribute('src', `${carousel.slides[carousel.current].url}`)
+}
+
+imageFetch()
 
 setInterval(render, 4000)
